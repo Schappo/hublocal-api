23f21e7cd9b14e522351a818bf9c42016da915bc
Felipe Schappo <felipeschappoo@gmail.com>
2023-02-16 04:15:27 -0300

    feat: encrypt password on create

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { Prisma, User } from '@prisma/client'
+import { encryptPassword } from '../helpers/encrypt.helper'
 import { PrismaService } from '../prisma.service'
 
 @Injectable()
@@ -8,9 +9,9 @@ export class UserService {
     private readonly prisma: PrismaService,
   ) { }
 
-  async create(data: Prisma.UserCreateInput): Promise<User> {
-    return this.prisma.user.create({
-      data,
-    })
+  async create(user: Prisma.UserCreateInput): Promise<User> {
+    const encryptedPassword = await encryptPassword(user.password)
+    user.password = encryptedPassword
+    return this.prisma.user.create({ data: user })
   }
 }
