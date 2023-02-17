4e5d93bbdb1ae3bf62fc02072d6e693f2d270612
Felipe Schappo <felipeschappoo@gmail.com>
2023-02-17 01:54:56 -0300

    feat: create a base crud service

---
+++ src/common/base-crud.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common"
+import { PrismaService } from "../prisma.service"
+
+@Injectable()
+export class BaseCrudService<T> {
+  constructor(
+    protected readonly prisma: PrismaService,
+    private readonly modelName: string
+  ) { }
+
+  async findAll(): Promise<T[]> {
+    return await this.prisma[this.modelName].findMany()
+  }
+
+  async findById(id: string): Promise<T> {
+    return await this.prisma[this.modelName].findUnique({ where: { id } })
+  }
+
+  async create(data: Omit<T, 'id'>): Promise<T> {
+    return await this.prisma[this.modelName].create({ data })
+  }
+
+  async update(id: string, data: Omit<T, 'id'>): Promise<T> {
+    return await this.prisma[this.modelName].update({ where: { id }, data })
+  }
+
+  async delete(id: string): Promise<T> {
+    return await this.prisma[this.modelName].delete({ where: { id } })
+  }
+
+}
