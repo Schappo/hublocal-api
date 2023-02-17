79fab82c4973750464513639db804d674ada1415
Felipe Schappo <felipeschappoo@gmail.com>
2023-02-17 16:39:09 -0300

    feat: create a company controller, service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppService } from './app.service'
 import { validate } from './common/config/env.validation'
 import { PrismaService } from './prisma.service'
 import { UserModule } from './user/user.module'
+import { CompanyModule } from './company/company.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     validate,
     isGlobal: true,
-  }), UserModule],
+  }), UserModule, CompanyModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
