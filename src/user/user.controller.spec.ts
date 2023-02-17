ce8e1eaad80df10253cb4266c0c996ed5873a892
Felipe Schappo <felipeschappoo@gmail.com>
2023-02-17 13:01:54 -0300

    test: create a user controller test

---
+++ src/user/user.controller.spec.ts
@@ -0,0 +1,63 @@
+import { faker } from '@faker-js/faker'
+import { INestApplication } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import * as request from 'supertest'
+import { randomUser } from '../common/helpers/faker.helper'
+import { PrismaService } from '../prisma.service'
+import { UserController } from './user.controller'
+import { UserService } from './user.service'
+
+
+describe('UserController', () => {
+  let userService: UserService
+  let app: INestApplication
+
+  const fakeUserInput = randomUser()
+  const user = {
+    ...fakeUserInput,
+    id: faker.datatype.uuid()
+  }
+  const { ['password']: remove, ...fakeUserWithoutPassword } = user
+
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UserController],
+      providers: [UserService, PrismaService],
+    }).compile()
+
+    app = module.createNestApplication()
+    app.init()
+    userService = module.get<UserService>(UserService)
+
+    jest.clearAllMocks()
+  })
+
+  it('should be defined', () => {
+    expect(userService).toBeDefined()
+  })
+
+  describe('Return User Without password', () => {
+    it('should create a user and return a user without password', async () => {
+
+      jest.spyOn(userService, 'create').mockResolvedValue(user)
+
+      const res = await request(app.getHttpServer())
+        .post('/user')
+        .send(fakeUserInput)
+        .expect(201)
+
+      expect(res.body).toEqual(fakeUserWithoutPassword)
+    })
+
+    it('should find a user by id and return a user without password', async () => {
+      jest.spyOn(userService, 'findById').mockResolvedValue(user)
+
+      const res = await request(app.getHttpServer())
+        .get(`/user/${fakeUserInput.id}`)
+        .expect(200)
+
+      expect(res.body).toEqual(fakeUserWithoutPassword)
+    })
+  })
+})
