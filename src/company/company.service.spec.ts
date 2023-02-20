f50e8a12ae2cab0410292eb5dfc68cdf943bd9bd
Felipe Schappo <felipeschappoo@gmail.com>
2023-02-20 20:11:22 -0300

    test: should create a Company

--- src/company/company.service.spec.ts
+++ src/company/company.service.spec.ts
@@ -1,18 +1,37 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CompanyService } from './company.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { Company, Prisma } from '@prisma/client'
+import { randomUUID } from 'crypto'
+import { randomCompany } from '../common/helpers'
+import { PrismaService } from '../prisma.service'
+import { CompanyService } from './company.service'
 
 describe('CompanyService', () => {
-  let service: CompanyService;
+  let service: CompanyService
+  let prisma: PrismaService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CompanyService],
-    }).compile();
+      providers: [CompanyService, PrismaService],
+    }).compile()
 
-    service = module.get<CompanyService>(CompanyService);
-  });
+    service = module.get<CompanyService>(CompanyService)
+    prisma = module.get<PrismaService>(PrismaService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+
+  it('should create a Company', async () => {
+    const company: Prisma.CompanyCreateInput = randomCompany({ hasCompanyId: true })
+
+    const expectedCompany = {
+      id: randomUUID(),
+      ...company
+    }
+
+    jest.spyOn(prisma.company, 'create').mockResolvedValue(expectedCompany as Company)
+    expect(service.create(company)).resolves.toEqual(expectedCompany)
+    expect(prisma.company.create).toHaveBeenCalledWith({ data: company })
+  })
+})
